6782e7a5e3e2188c167bf47753b2b5af61c423f4
Steve M Silva <silvasm516@aol.com>
2021-09-27 06:06:00 -0700

    icon code to base

--- ddbapp/urls.py
+++ ddbapp/urls.py
@@ -3,12 +3,12 @@ from .views import Footer7View, calculate, DEPView, doubledb, syd, straightline
 from . import views
 
 urlpatterns = [
-    path( '', DEPView.as_view(), name= "index"),
+    path( 'index', DEPView.as_view(), name= "index"),
     path('Footer7/', Footer7View.as_view(), name= 'Footer7'),
     path('Z', doubledb, name= 'doubledb'),
     path('X', syd, name= 'syd'),
     path('Y', straightline, name= 'straightline'),
     path('A', calculate, name= 'calculate'),
-    # path('static', manifest, name='manifest')
+    #path('static', manifest, name='manifest')
     
 ]
