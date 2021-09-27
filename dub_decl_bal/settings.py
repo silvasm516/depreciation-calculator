16513abee9e46d1085011161519ef1a116b083b2
Steve M Silva <silvasm516@aol.com>
2021-09-27 06:24:55 -0700

    more settings

--- dub_decl_bal/settings.py
+++ dub_decl_bal/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'tr9me&+wbl$_$ltb$_o98k0@=zki713n*-%9efel@o8u-o3z2n'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1, secure-citadel-03902.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'secure-citadel-03902.herokuapp.com']
 
 
 # Application definition
