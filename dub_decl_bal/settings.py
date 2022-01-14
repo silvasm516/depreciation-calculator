72250717f36e7edb2529173b2ab153e66b30ac52
Steve M Silva <silvasm516@aol.com>
2022-01-14 09:33:06 -0800

    various

--- dub_decl_bal/settings.py
+++ dub_decl_bal/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'tr9me&+wbl$_$ltb$_o98k0@=zki713n*-%9efel@o8u-o3z2n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 #ALLOWED_HOSTS = ['secure-citadel-03902.herokuapp.com, 127.0.0.1, 127.0.0.1:8000, localhost']
 ALLOWED_HOSTS = ['*']
